dd78a1cf87504f4fcc7381af7db32471e22d5087
theapache64 <theapache64@gmail.com>
2021-06-21 20:08:03 +0530

    🌟 add ViewExt

---
+++ files/ViewExt.kt
@@ -0,0 +1,15 @@
+package $PACKAGE_NAME.util.extensions
+
+import android.view.View
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
