aa364355a4bf52e31dae3cd4e95fd496cde9b5de
theapache64 <theapache64@gmail.com>
2021-08-23 19:36:21 +0530

    ◀️ make BigColumn public

--- files/BigColumn.kt
+++ files/BigColumn.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
-private fun BigColumn(
+fun BigColumn(
     color: Color,
     content: @Composable () -> Unit
 ) {
