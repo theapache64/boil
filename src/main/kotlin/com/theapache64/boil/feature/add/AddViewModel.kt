502ef3220c8852ecd5b52870ec8d05319298b220
theapache64 <theapache64@gmail.com>
2021-09-10 23:11:47 +0530

    🌟 add signature to kt files

--- src/main/kotlin/com/theapache64/boil/feature/add/AddViewModel.kt
+++ src/main/kotlin/com/theapache64/boil/feature/add/AddViewModel.kt
@@ -20,6 +20,23 @@ class AddViewModel @Inject constructor(
 ) : BaseViewModel<AddView>() {
 
     companion object {
+        private val KOTLIN_SIGNATURE by lazy {
+            """/*
+ * Copyright 2021 Boil (https://github.com/theapache64/boil)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */""".trimIndent()
+        }
         private const val ENV_VAR_PACKAGE_NAME = "${'$'}PACKAGE_NAME"
 
         private val SUPPORTED_TEXT_FILES = arrayOf(
@@ -123,8 +140,8 @@ class AddViewModel @Inject constructor(
 
             var fileName = file.key
             val fileContent = file.value
-            val updatedFileContent = fileContent.replace(ENV_VAR_PACKAGE_NAME, packageName)
             val fileExt = File(fileName).extension
+            val updatedFileContent = fileContent.replace(ENV_VAR_PACKAGE_NAME, packageName)
 
             if (SUPPORTED_TEXT_FILES.contains(fileExt)) {
                 // text file
@@ -154,7 +171,8 @@ class AddViewModel @Inject constructor(
                 if (targetFile.exists()) {
                     println("${targetFile.absolutePath} exists!")
                 } else {
-                    targetFile.writeText(updatedFileContent)
+                    val signaturedFileContent = addSignature(updatedFileContent, fileExt)
+                    targetFile.writeText(signaturedFileContent)
                     println("➡️ Created ${targetFile.absolutePath}")
                 }
 
@@ -166,6 +184,15 @@ class AddViewModel @Inject constructor(
         }
 
         println("👍 Integration finished")
+    }
+
+    private fun addSignature(fileContent: String, fileExt: String): String {
+        return when (fileExt) {
+            "kt" -> {
+                "${KOTLIN_SIGNATURE}\n$fileContent"
+            }
+            else -> fileContent
+        }
     }
 
     private suspend fun downloadFiles(classList: List<String>): Map<String, String> {
