bc254271d3d2aba45521f3e36e09355f53496388
theapache64 <theapache64@gmail.com>
2021-09-24 14:06:20 +0530

    🌟 add LogComposition

---
+++ files/LogComposition.kt
@@ -0,0 +1,24 @@
+package $PACKAGE_NAME.util
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+
+class Ref(var value: Int)
+
+/**
+ * Note the inline function below which ensures that this function is essentially
+ * copied at the call site to ensure that its logging only recompositions from the
+ * original call site.
+ */
+@Composable
+@Suppress("NOTHING_TO_INLINE")
+inline fun LogCompositions(tag: String, msg: String) {
+
+    if (BuildConfig.DEBUG) {
+        val ref = remember { Ref(0) }
+        SideEffect { ref.value++ }
+        Log.d(tag, "Compositions: ${msg} ${ref.value}")
+    }
+}
